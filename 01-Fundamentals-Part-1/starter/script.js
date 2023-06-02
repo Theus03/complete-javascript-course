0da086aa628eb2375b13697be5546b632cc38aee
Matheus Leite <matheus.leite@3stecnologia.com.br>
2023-06-02 12:00:48 -0300

    Execise one e two done of fundamentals part one.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,66 @@
+// CLASS
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let myFirstJob = "Programemr";
+let myCurretJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "techer";
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+console.log()
+
+// PRACTICE
+
+let country = "Brazil";
+let continent = "South America";
+let population = 214.3;
+let isIsland = true;
+let language;
+
+if (country === "Island"){
+    console.log(isIsland)
+} else {
+    isIsland = false;
+    console.log('The country is island? ' + isIsland)
+    console.log(`Country: ${country} - Continent: ${continent} - Poppulation: ${population} milions`);
+}
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
+
