76bcae010e9372a2b925c49d0f53b19252d622c1
Theus03 <matheusssleite@outlook.com>
2023-06-08 18:15:57 -0300

    Finish class strings template

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -60,7 +60,6 @@ job = 'teacher';
 
 lastName = 'Leite'; // Não é uma boa prática
 console.log(lastName);
-*/
 
 const now = 2037;
 const ageJonas = now - 1991;
@@ -87,6 +86,25 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+
+*/
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + 'years old' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log(`String with
+multiple
+lines`);
 
 // PRACTICE
 
